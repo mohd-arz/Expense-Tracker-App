60dc17e5d354c9595f1c4b655f15817d64cf0aad
mohd-arz <mohammedarsh75@gmail.com>
2023-04-17 13:28:56 +0530

    test

---
+++ app/src/main/java/com/example/expensestracker/MainActivity.kt
@@ -0,0 +1,145 @@
+package com.example.expensestracker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.expensestracker.ui.theme.ExpensesTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Scaffold(
+                // in scaffold we are specifying top bar.
+                bottomBar = {
+                    // inside top bar we are specifying
+                    // background color.
+                    BottomAppBar(backgroundColor = Color.Transparent,
+                        modifier = Modifier.height(80.dp),
+                        // along with that we are specifying
+                        // title for our top bar.
+                        content = {
+
+                            Spacer(modifier = Modifier.width(15.dp))
+
+                            Button(
+                                onClick = {startActivity(Intent(applicationContext,AddExpensesActivity::class.java))},
+                                colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray),
+                                modifier = Modifier.size(height = 55.dp, width = 110.dp)
+                            )
+                            {
+                                Text(
+                                    text = "Add Expenses", color = Color.LightGray, fontSize = 14.sp,
+                                    textAlign = TextAlign.Center
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.width(15.dp))
+                            
+                            Button(
+                                onClick = {
+                                    startActivity(
+                                        Intent(
+                                            applicationContext,
+                                            SetLimitActivity::class.java
+                                        )
+                                    )
+                                },
+                                colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray),
+                                modifier = Modifier.size(height = 55.dp, width = 110.dp)
+                            )
+                            {
+                                Text(
+                                    text = "Set Limit", color = Color.LightGray, fontSize = 14.sp,
+                                    textAlign = TextAlign.Center
+                                )
+                            }
+
+                            Spacer(modifier = Modifier.width(15.dp))
+
+                            Button(
+                                onClick = {
+                                    startActivity(
+                                        Intent(
+                                            applicationContext,
+                                            ViewRecordsActivity::class.java
+                                        )
+                                    )
+                                },
+                                colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray),
+                                modifier = Modifier.size(height = 55.dp, width = 110.dp)
+                            )
+                            {
+                                Text(
+                                    text = "View Records", color = Color.LightGray, fontSize = 14.sp,
+                                    textAlign = TextAlign.Center
+                                )
+                            }
+
+                        }
+                    )
+                }
+            ) {
+                MainPage()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainPage() {
+//    Column(
+//        modifier = Modifier.padding(20.dp).fillMaxSize(),
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//
+//
+//        Image(painterResource(id = R.drawable.img_1), contentDescription ="", modifier = Modifier.size(height = 500.dp, width = 500.dp))
+
+        Box(
+            modifier = with (Modifier){
+                fillMaxSize()
+                    .paint(
+                        // Replace with your image id
+                        painterResource(id = R.drawable.chart_g0e8c91eea_1920),
+                        contentScale = ContentScale.FillHeight)
+            }){
+            Column(
+        modifier = Modifier.padding(20.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+                Text(
+                    text = "Welcome To Expense Tracker",
+                    fontSize = 42.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    color = Color.LightGray,
+                    fontFamily = FontFamily.Default
+                )
+            }
+        }
+    }
